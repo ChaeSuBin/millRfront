b4ffbebe8baeb348f9ce5fcec6a41e335cee23ad
GOGACOLA\chae6 <k96765435@gmail.com>
2023-01-24 04:03:25 +0900

    'trash24'

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 async function request(path, options = {}) {
-    const url = `https://mintservice.asuscomm.com:8139${path}`;
-    // const url = `https://gc4t8kvc7c.execute-api.us-east-1.amazonaws.com${path}`;
-    const response = await fetch(url, options);
-    return response.json();
+  const url = `https://mintservice.asuscomm.com:8139${path}`;
+  // const url = `https://gc4t8kvc7c.execute-api.us-east-1.amazonaws.com${path}`;
+  const response = await fetch(url, options);
+  return response.json();
 }
 
 export async function getMatchCode(_userId, _vcode){
@@ -18,113 +18,113 @@ export async function getOwnedItems(_userId) {
   return request(`/getowneditem/${_userId}`);
 }
 export async function getOwnedTokns(_userId) {
-  return request(`/getownedtokn/${_userId}`);
+return request(`/getownedtokn/${_userId}`);
 }
 export async function getItemTitle(_toknId) {
-  return request(`/getitemtitle/${_toknId}`);
+return request(`/getitemtitle/${_toknId}`);
 }
 export async function getItemsIdList(_userId, _itemHash) {
-  return request(`/getidlist/${_userId}/${_itemHash}`);
+return request(`/getidlist/${_userId}/${_itemHash}`);
 }
 export async function getChainId(_userId) {
-  return request(`/chainacc/${_userId}`);
+return request(`/chainacc/${_userId}`);
 }
 export async function getMerge(_fileName, _fileHash) {
-  return request(`/merge/${_fileName}/${_fileHash}`);
+return request(`/merge/${_fileName}/${_fileHash}`);
 }
 export async function getItem(_rowId) {
-  return request(`/iteminfo/${_rowId}`);
+return request(`/iteminfo/${_rowId}`);
 }
 export async function getToknId(_fromAddr){
-  return request(`/toknid/${_fromAddr}`);
+return request(`/toknid/${_fromAddr}`);
 }
 export async function getDonateList(){
-  return request('/donatelist');
+return request('/donatelist');
 }
-export async function getOpenedItem(){
-  return request('/openeditems');
+export async function getOpenedItem(_page){
+return request(`/openeditemsnew/${_page}`);
 }
 export async function getOpenedTokn(){
-  return request('/openedtokns');
+return request('/openedtokns');
 }
 export async function getHash(_toknId) {
-  return request(`/toknhash/${_toknId}`);
+return request(`/toknhash/${_toknId}`);
 }
 export async function getFileList(_toknUri) {
-  return request(`/dirsync/${_toknUri}`);
+return request(`/dirsync/${_toknUri}`);
 }
 export async function getFileBolb(_dirName, _fileName){
-  const url = `https://mintservice.asuscomm.com:8139/downpath/${_dirName}/${_fileName}`;
-  const response = await fetch(url);
-  return response.blob();
+const url = `https://mintservice.asuscomm.com:8139/downpath/${_dirName}/${_fileName}`;
+const response = await fetch(url);
+return response.blob();
 }
 export async function putChainId(record) {
-  return request(`/setchainid`, {
-    body: JSON.stringify(record),
-    headers: {"Content-Type": "application/json"},
-    method: "PUT",
-  });
+return request(`/setchainid`, {
+  body: JSON.stringify(record),
+  headers: {"Content-Type": "application/json"},
+  method: "PUT",
+});
 }
 export async function putItemClose(record) {
-  return request(`/setitemclose`, {
-    body: JSON.stringify(record),
-    headers: {"Content-Type": "application/json"},
-    method: "PUT",
-  });
+return request(`/setitemclose`, {
+  body: JSON.stringify(record),
+  headers: {"Content-Type": "application/json"},
+  method: "PUT",
+});
 }
 export async function putToknSale(record) {
-  return request(`/settoknsale`, {
-    body: JSON.stringify(record),
-    headers: {"Content-Type": "application/json"},
-    method: "PUT",
-  });
+return request(`/settoknsale`, {
+  body: JSON.stringify(record),
+  headers: {"Content-Type": "application/json"},
+  method: "PUT",
+});
 }
 export async function buyedToknChange(record) {
-  return request(`/buytoknchange`, {
-    body: JSON.stringify(record),
-    headers: {"Content-Type": "application/json"},
-    method: "PUT",
-  });
+return request(`/buytoknchange`, {
+  body: JSON.stringify(record),
+  headers: {"Content-Type": "application/json"},
+  method: "PUT",
+});
 }
 export async function putLogin(record) {
-  return request(`/usercheckin`, {
-    body: JSON.stringify(record),
-    headers: {"Content-Type": "application/json"},
-    method: "PUT",
-  });
+return request(`/usercheckin`, {
+  body: JSON.stringify(record),
+  headers: {"Content-Type": "application/json"},
+  method: "PUT",
+});
 }
 export async function postRegiPlayer(record) {
-  return request(`/regiplayer`, {
-    body: JSON.stringify(record),
-    headers: {"Content-Type": "application/json"},
-    method: "POST",
-  });
+return request(`/regiplayer`, {
+  body: JSON.stringify(record),
+  headers: {"Content-Type": "application/json"},
+  method: "POST",
+});
 }
 export async function postTempuserTable(record) {
-  return request(`/createtempuser`, {
-    body: JSON.stringify(record),
-    headers: {"Content-Type": "application/json"},
-    method: "POST",
-  });
+return request(`/createtempuser`, {
+  body: JSON.stringify(record),
+  headers: {"Content-Type": "application/json"},
+  method: "POST",
+});
 }
 export async function postUploadArtwork(record) {
-  return request(`/fileupload`, {
-    body: JSON.stringify(record),
-    headers: {"Content-Type": "application/json"},
-    method: "POST",
-  });
+return request(`/fileupload`, {
+  body: JSON.stringify(record),
+  headers: {"Content-Type": "application/json"},
+  method: "POST",
+});
 }
 export async function postUploadItemIdx(record) {
-  return request(`/fileinfoupload`, {
-    body: JSON.stringify(record),
-    headers: {"Content-Type": "application/json"},
-    method: "POST",
-  });
+return request(`/fileinfoupload`, {
+  body: JSON.stringify(record),
+  headers: {"Content-Type": "application/json"},
+  method: "POST",
+});
 }
 export async function postMintTokn(record) {
-  return request(`/minttoknidx`, {
-    body: JSON.stringify(record),
-    headers: {"Content-Type": "application/json"},
-    method: "POST",
-  });
+return request(`/minttoknidx`, {
+  body: JSON.stringify(record),
+  headers: {"Content-Type": "application/json"},
+  method: "POST",
+});
 }
--- src/pages/rootPage.js
+++ src/pages/rootPage.js
@@ -5,16 +5,18 @@ import { ListItemsCpnt } from "../components/itemCpnt";
 
 export const HomePage = () => {
   const [opendList, setOpend] = useState([]);
+  const [pageNum, setPage] = useState(1);
+  const [allItemRows, setRows] = useState(0);
 
   useEffect(() => {
-    getOpenedItems();
-    //window.location.reload();
+    getOpenedItems(1);
   },[]);
   
-  const getOpenedItems = async() => {
-    const itemInfo = await getOpenedItem();
-    console.log(itemInfo);
-    setOpend(itemInfo);
+  const getOpenedItems = async(_pageNum) => {
+    const itemInfo = await getOpenedItem(_pageNum);
+    // console.log(itemInfo[0]);
+    setRows(itemInfo[1]);
+    setOpend(itemInfo[0]);
   }
   const getOpenedTokns = async() => {
     let toknList = [];
@@ -33,13 +35,26 @@ export const HomePage = () => {
   const refreshPage = () => {
     window.location.assign('./filetohash');
   }
+  const nextPage = (_page) => {
+    if(allItemRows > _page*5){
+      setPage(pageNum+1);
+      getOpenedItems(_page+1);
+    }
+  }
+  const prevPage = (_page) => {
+    console.log(_page);
+    if(_page > 1){
+      setPage(pageNum-1);
+      getOpenedItems(_page-1);
+    }
+  }
   return(
     <>
       <p style={{textAlign: "center"}}>
       <Link to="/filetohash" style={{ textDecoration: 'none' }}>
         <button onClick={refreshPage} style={{fontSize:"large"}}>아이템 업로드</button>
       </Link></p>
-      <label><input type="radio" onChange={getOpenedItems} name='comm' defaultChecked/>
+      <label><input type="radio" onChange={()=>getOpenedItems(pageNum)} name='comm' defaultChecked/>
         초기판매 아이템 목록 </label>
       <label><input type="radio" onChange={getOpenedTokns} name='comm'/>
         2차거래 NFT 목록 </label><br/>
@@ -52,7 +67,9 @@ export const HomePage = () => {
           itemId = {searchItems.id}
         />
       ))}
-      
+      <a onClick={()=>prevPage(pageNum)} style={{cursor: "pointer"}} >prev⬅️</a>
+      {' '+pageNum+' '}
+      <a onClick={()=>nextPage(pageNum)} style={{cursor: "pointer"}} >➡️next</a>
     </>
   )
 }
